fa2560d8ca564a6436bab512865883f3cd1e30b8
OrangesAL <1263107985@qq.com>
2019-04-01 15:30:34 +0800

    spec add a

--- IBProperty/Basis/UILabel+ORIBPropertyBasis.h
+++ IBProperty/Basis/UILabel+ORIBPropertyBasis.h
@@ -11,6 +11,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UILabel (ORIBPropertyBasis)
 
+/*
+ * 根据屏幕的宽度 适配 字体大小
+ * adapt font size by screen width
+ */
+@property (nonatomic, assign) IBInspectable BOOL ib_adaptFont;
+
 @end
 
 NS_ASSUME_NONNULL_END
